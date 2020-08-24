2688b8a294fdaf19c82e9166c5445defc7871e9e
coding-by-feng <1215587087@qq.com>
2020-08-24 16:16:26 +0800

    init

---
+++ src/me/fengorz/leetcode/Test.java
@@ -0,0 +1,117 @@
+package me.fengorz.leetcode;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Description TODO
+ * @Author zhanshifeng
+ * @Date 2020/8/3 9:16 PM
+ */
+public class Test {
+
+    public static void main(String[] args) throws InterruptedException {
+        // FooBarTest();
+        // zeroEvenOddTest();
+        h2o();
+    }
+
+    private static void FooBarTest() {
+        FooBar fooBar = new FooBar(100);
+        Runnable foo = () -> {
+            System.out.print("foo");
+        };
+        Runnable bar = () -> {
+            System.out.print("bar");
+        };
+        Thread thread1 = new Thread(() -> {
+            try {
+                fooBar.foo(foo);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            try {
+                fooBar.bar(bar);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+
+    private static void zeroEvenOddTest() {
+        ZeroEvenOdd test = new ZeroEvenOdd(100);
+        Runnable even = () -> {
+            try {
+                test.even(System.out::print);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Runnable odd = () -> {
+            try {
+                test.odd(System.out::print);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Runnable zero = () -> {
+            try {
+                test.zero(System.out::print);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Thread thread1 = new Thread(even);
+        Thread thread2 = new Thread(odd);
+        Thread thread3 = new Thread(zero);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+
+    private static void h2o() {
+        // BlockingQueue<String> queue = new ArrayBlockingQueue<String>(100);
+        // String[] arr = new String[]{"H", "O", "H", "O", "H", "H", "H", "O", "H", "O", "H", "H", "H", "H", "O"};
+        // Arrays.stream(arr).peek(queue::add);
+        H2O test = new H2O();
+        Runnable h = () -> {
+            AtomicInteger i = new AtomicInteger(100);
+            while (i.get() > 0) {
+                try {
+                    test.hydrogen(() -> {
+                        System.out.print("H");
+                        i.getAndDecrement();
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Runnable o = () -> {
+            AtomicInteger i = new AtomicInteger(50);
+            while (i.get() > 0) {
+                try {
+                    test.oxygen(() -> {
+                        System.out.print("O");
+                        i.getAndDecrement();
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread t1 = new Thread(h);
+        Thread t2 = new Thread(o);
+
+        t2.start();
+        t1.start();
+    }
+
+}
---
+++ src/me/fengorz/leetcode/ZeroEvenOdd.java
@@ -0,0 +1,67 @@
+package me.fengorz.leetcode;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.IntConsumer;
+
+public class ZeroEvenOdd {
+    private int n;
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition zeroCondition = lock.newCondition();
+    private final Condition evenCondition = lock.newCondition();
+    private final Condition oddCondition = lock.newCondition();
+    private final AtomicBoolean zeroPrint = new AtomicBoolean();
+    private final AtomicBoolean oddPrint = new AtomicBoolean();
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            this.lock.lock();
+            if (zeroPrint.get()) {
+                this.zeroCondition.await();
+            }
+            printNumber.accept(0);
+            zeroPrint.compareAndSet(false, true);
+            if (oddPrint.get()) {
+                this.evenCondition.signalAll();
+            } else {
+                this.oddCondition.signalAll();
+            }
+            this.lock.unlock();
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            this.lock.lock();
+            if (!zeroPrint.get() || !oddPrint.get()) {
+                this.evenCondition.await();
+            }
+            printNumber.accept(i);
+            zeroPrint.compareAndSet(true, false);
+            oddPrint.compareAndSet(true, false);
+            this.zeroCondition.signalAll();
+            this.lock.unlock();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            this.lock.lock();
+            if (!zeroPrint.get() || oddPrint.get()) {
+                this.oddCondition.await();
+            }
+            printNumber.accept(i);
+            zeroPrint.compareAndSet(true, false);
+            oddPrint.compareAndSet(false, true);
+            this.zeroCondition.signalAll();
+            this.lock.unlock();
+        }
+    }
+}
