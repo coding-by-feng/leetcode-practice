da77205ca7ed23d9bf543b93b2c19bf3c408366a
coding-by-feng <1215587087@qq.com>
2020-09-16 21:51:24 +0800

    Sum of Left Leaves.

---
+++ src/me/fengorz/leetcode/tree/sum_of_left_leaves/Solution.java
@@ -0,0 +1,73 @@
+package me.fengorz.leetcode.tree.sum_of_left_leaves;//计算给定二叉树的所有左叶子之和。
+//
+// 示例： 
+//
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24 
+//
+// 
+// Related Topics 树 
+// 👍 190 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+
+    /**
+     * 超简单的普通递归解法、
+     *
+     * @param root
+     * @return
+     */
+    private int sum = 0;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        next(root, false);
+        return sum;
+    }
+
+    private void next(TreeNode node, boolean isLeft) {
+        if (node == null) {
+            return;
+        }
+        if (node.left == null && node.right == null) {
+            if (isLeft) {
+                sum += node.val;
+            }
+            return;
+        }
+        if (node.left != null) {
+            next(node.left, true);
+        }
+        if (node.right != null) {
+            next(node.right, false);
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
